bfc29347e38af32a62b2bc2031d3fb840e72bb18
NdunguKariuki1 <3560.2021@students.ku.ac.ke>
2024-06-07 09:14:45 +0300

    javascript basics

---
+++ variables.js
@@ -0,0 +1,7 @@
+let  name='Austine'; //String literal
+let age=30; //Number literal
+let isApproved = false; //Boolean literal
+let firstName = undefined;
+let lastName = null;
+
+//Examples of primitive types of variables
