dd106e5e4bdf287fc9b50361700014283b024719
NdunguKariuki1 <3560.2021@students.ku.ac.ke>
2024-06-19 08:00:12 +0300

    final

--- variables.js
+++
@@ -1,7 +0,0 @@
-let  name='Austine'; //String literal
-let age=30; //Number literal
-let isApproved = false; //Boolean literal
-let firstName = undefined;
-let lastName = null;
-
-//Examples of primitive types of variables
